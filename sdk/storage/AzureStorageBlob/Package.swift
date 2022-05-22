da48ff273b0ecdc5998edffc272cbe572023b7a7
Alessandro Pozone <poz1@users.noreply.github.com>
2022-05-22 15:54:54 +0200

    Create Package.swift

---
+++ sdk/storage/AzureStorageBlob/Package.swift
@@ -0,0 +1,63 @@
+// swift-tools-version:5.3
+//  The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// --------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// The MIT License (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the ""Software""), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+// --------------------------------------------------------------------------
+import PackageDescription
+
+let package = Package(
+    name: "AzureStorageBlob",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v12)
+    ],
+    products: [
+        .library(name: "AzureStorageBlob", targets: ["AzureStorageBlob"])
+    ],
+    targets: [
+        // Build targets
+        .target(
+            name: "AzureStorageBlob",
+            dependencies: [],
+            path: "Source",
+            exclude: [
+                "README.md",
+                "Tests",
+                "Source/Supporting Files",
+                "LICENSE"
+            ]
+        ),
+        // Test targets
+        .testTarget(
+            name: "AzureStorageBlobTests",
+            dependencies: ["AzureStorageBlob"],
+            path: "Tests",
+            exclude: [
+                "Info.plist"
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
